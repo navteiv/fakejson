69817a3108ce08cc06121844f765f93cdfe29af7
Van <vannv0820@gmail.com>
2021-09-07 16:54:21 +0700

    add component

---
+++ src/app/httpclientdemo/httpclientdemo.component.spec.ts
@@ -0,0 +1,25 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { HttpclientdemoComponent } from './httpclientdemo.component';
+
+describe('HttpclientdemoComponent', () => {
+  let component: HttpclientdemoComponent;
+  let fixture: ComponentFixture<HttpclientdemoComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [ HttpclientdemoComponent ]
+    })
+    .compileComponents();
+  });
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(HttpclientdemoComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
---
+++ src/app/httpclientdemo/httpclientdemo.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-httpclientdemo',
+  templateUrl: './httpclientdemo.component.html',
+  styleUrls: ['./httpclientdemo.component.css']
+})
+export class HttpclientdemoComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
